57e026eca7dad704bdb372ac8d7cf00420340f79
Romain ACCIARI <romain.acciari@gmail.com>
2015-09-21 21:25:42 +0200

    Add configuration files and fix deployment

--- predeploy.sh
+++ predeploy.sh
@@ -1,4 +1,4 @@
-VAGRANT_MAIN_VM=$2
+VAGRANT_SWIFT_IP=$2
 
 # Check IP argument
 if [ $# -ne 2 ]; then
@@ -7,14 +7,21 @@ if [ $# -ne 2 ]; then
   exit 1
 fi
 # Check if IP is valid
-$IPCALC -s -c "$VAGRANT_MAIN_VM"
+$IPCALC -s -c "$VAGRANT_SWIFT_IP"
 if [ $? -ne 0 ]; then
-  echo "Error: IP $VAGRANT_MAIN_VM is invalid."
+  echo "Error: IP $VAGRANT_SWIFT_IP is invalid."
   echo "Usage: $0 $1 <ip_address>"
   exit 1
 fi
 
-# Switching vagrant main IP into manifests
-$SED -i -e "s@VAGRANT_MAIN_VM@$VAGRANT_MAIN_VM@g" /root/pydiodb.sql
-/bin/mysql pydiodb </root/pydiodb.sql
+# Retrieving demo user tenant ID
+$SED -i -e "s@VAGRANT_SWIFT_IP@$VAGRANT_SWIFT_IP@g" /usr/share/puppet/modules/openiosds/profiles/vagrant-ext-pydio/conf/keystonerc_demo
+source /usr/share/puppet/modules/openiosds/profiles/vagrant-ext-pydio/conf/keystonerc_demo
+VAGRANT_TENANT_ID=$(/bin/keystone token-get | /bin/awk '/tenant_id/ {print $4}')
+
+# Switching vagrant main IP and tenant ID in the Pydio database configuration
+$SED -i -e "s@VAGRANT_SWIFT_IP@$VAGRANT_SWIFT_IP@g" -e "s@VAGRANT_TENANT_ID@$VAGRANT_TENANT_ID@g" /usr/share/puppet/modules/openiosds/profiles/vagrant-ext-pydio/conf/pydiodb.sql
+
+# Load Pydio database
+/bin/mysql pydiodb </usr/share/puppet/modules/openiosds/profiles/vagrant-ext-pydio/conf/pydiodb.sql
 
